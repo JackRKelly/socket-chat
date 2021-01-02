7690319dab39590850597fbaf36cb5a1feaa246f
JackRKelly <kcjackkelly@gmail.com>
2021-01-02 12:53:36 -0600

    Added support for .env

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import { Message, JoinSocket, JoinChat } from "shared/src";
 import cors from "cors";
 
+require("dotenv").config();
 const app = express();
 const http = require("http").createServer(app);
 const socketIO = require("socket.io");
@@ -16,12 +17,10 @@ const io = socketIO(http, {
 app.get("/", function (req: Request, res: Response) {
   res.send("Hello Socket");
 });
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5000" }));
 
 io.on("connection", (socket: JoinSocket) => {
   console.log("User has connected.");
-
-  socket.emit("request join");
 
   socket.on("join chat", (join: JoinChat) => {
     //Emit message to all users including sender
@@ -35,6 +34,8 @@ io.on("connection", (socket: JoinSocket) => {
     console.log(`${join.uid} has joined the chat`);
   });
 
+  socket.emit("request join");
+
   socket.on("message", (msg: Message) => {
     //Emit message to all users including sender
     msg.uid = socket.uid;
@@ -47,6 +48,7 @@ io.on("connection", (socket: JoinSocket) => {
   });
 });
 
-http.listen(3000, () => {
-  console.log("Server listening on port 3000");
+const port = process.env.PORT || 8080;
+http.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
